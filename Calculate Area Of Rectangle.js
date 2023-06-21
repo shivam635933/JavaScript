99f919f296469c7d3a22afe40207467539b0fbb0
shivam635933 <67838305+shivam635933@users.noreply.github.com>
2023-06-21 15:30:44 +0530

    Create Calculate Area Of Rectangle.js
    Program to Calculate Area of Rectangle.
    
    Note: Take Input from the User

---
+++ Calculate Area Of Rectangle.js
@@ -0,0 +1,17 @@
+//To Write a JavaScript function called "calculateArea" that takes two parameters: "length" and "width". 
+//The function should calculate and return the area of a rectangle using the given length and width.
+
+function calculateArea(length,width){
+      
+      return length*width; //To Calculate Area of Rectangle
+      
+      //or we can use
+      //Area = length*width;
+      //return Area;
+}
+
+var length = parseInt(prompt("Enter Length of the Rectangle: ")); //prompt is used to take input(in string form) and parseInt is used to convert string into int
+var width = parseInt(prompt("Enter Width of the Rectangle: "));
+
+var Area = calculateArea(length,width);//Area to store the value of the return value of the function
+console.log("Area of Rectangle is: "+Area); //To Print the Area of Rectangle
