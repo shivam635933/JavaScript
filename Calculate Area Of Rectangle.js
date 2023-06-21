a3acd8647ea9b30304e6651072c925d1d773aac5
shivam635933 <67838305+shivam635933@users.noreply.github.com>
2023-06-21 15:34:08 +0530

    Update Calculate Area Of Rectangle.js

--- Calculate Area Of Rectangle.js
+++ Calculate Area Of Rectangle.js
@@ -1,6 +1,7 @@
 //To Write a JavaScript function called "calculateArea" that takes two parameters: "length" and "width". 
 //The function should calculate and return the area of a rectangle using the given length and width.
 
+//Function Declaration
 function calculateArea(length,width){
       
       return length*width; //To Calculate Area of Rectangle
@@ -13,5 +14,12 @@ function calculateArea(length,width){
 var length = parseInt(prompt("Enter Length of the Rectangle: ")); //prompt is used to take input(in string form) and parseInt is used to convert string into int
 var width = parseInt(prompt("Enter Width of the Rectangle: "));
 
+//Invoking Function
 var Area = calculateArea(length,width);//Area to store the value of the return value of the function
 console.log("Area of Rectangle is: "+Area); //To Print the Area of Rectangle
+
+
+//Function to Calculate Perimeter of the Rectangle
+//function calculatePerimeter(length,width) {
+   //return 2*(length+width);
+//}
